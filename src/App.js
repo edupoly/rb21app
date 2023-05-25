81cfc51fc357ad103ad35576906220d62c0ab03f
edupoly <info.edupoly@gmail.com>
2023-05-25 10:14:23 +0530

    sdf

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Controlled from './Controlled';
 
 function App() {
   return (
-    <div >
-      
+    <div className='border border-2 border-danger m-2 p-4'>
+      <Controlled></Controlled>
     </div>
   );
 }
---
+++ src/Controlled.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+function Controlled() {
+
+    var rf1 = React.useRef();
+    var rf2 = React.useRef();
+    var rf3 = React.useRef();
+    function add(){
+        var n1 = +rf1.current.value;
+        var n2 = +rf2.current.value;
+        var result = n1+n2;
+        document.getElementById("res").innerHTML=result;
+    }
+    function checkEnter(e){
+        if(e.key==='Enter'){
+            rf2.current.focus();
+        }
+    }
+    function checkEnter2(e){
+        if(e.key==='Enter'){
+            rf3.current.focus();
+        }
+    }
+  return (
+    <div className='border border-2 border-success m-2 p-4'>
+        <h1>Controlled Component</h1>
+        <input type="text" ref={rf1} onKeyUp={checkEnter}/><br />
+        <input type="text" ref={rf2} onKeyUp={checkEnter2}/><br />
+        <button onClick={add} ref={rf3}>Add</button>
+        <h1>Result:<span id='res'></span></h1>
+    </div>
+  )
+}
+
+export default Controlled
