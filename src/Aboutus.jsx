729486323aca61b3e6a330337f9d67ccd6eeaec9
edupoly <info.edupoly@gmail.com>
2023-06-06 09:36:10 +0530

    sdfsdf

---
+++ src/Aboutus.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Aboutus() {
+  return (
+    <div>
+        <h1>Aboutus 
+            we are training React
+        </h1>
+    </div>
+  )
+}
+
+export default Aboutus
--- src/index.js
+++ src/index.js
@@ -3,12 +3,40 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Aboutus from './Aboutus';
+import Contactus from './Contactus';
+var myrouter = createBrowserRouter([
+  
+  {
+    path:'/abc',
+    element:<h1>Hello abc</h1>
+  },
+  {
+    path:'/',
+    element:<App/>,
+    children:[
+      {
+        path:'/aboutus',
+        element:<Aboutus></Aboutus>
+      },
+      {
+        path:'/contactus',
+        element:<Contactus></Contactus>
+      },
+    ]
+  },
+  {
+    path:'/xyz',
+    element:<h3>Welcome to India</h3>
+  }
+])
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={myrouter}></RouterProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
